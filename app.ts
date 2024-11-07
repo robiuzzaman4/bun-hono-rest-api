b5891075c5581d29ab271fa1dece289c10d0fad4
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-11-07 13:19:40 +0600

    feat: add hono cors configuration

--- app.ts
+++ app.ts
@@ -1,7 +1,13 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { notesRoutes } from "./routes/notes";
+
 const app = new Hono();
 
+// cors configuration
+app.use("*", cors());
+
+// test route to ensure everything is working properly
 app.get("/test", (c) =>
   c.json({
     success: true,
@@ -10,6 +16,6 @@ app.get("/test", (c) =>
 );
 
 // all notes routes
-app.route("api/notes", notesRoutes);
+app.route("/api/notes", notesRoutes);
 
 export default app;
--- routes/notes.ts
+++ routes/notes.ts
@@ -3,10 +3,14 @@ import { Hono } from "hono";
 export const notesRoutes = new Hono();
 notesRoutes.get("/", (c) => {
   return c.json({
+    success: true,
+    status: 200,
     notes: [
       { id: 1, title: "Note 1", content: "Content 1" },
       { id: 2, title: "Note 2", content: "Content 2" },
       { id: 3, title: "Note 3", content: "Content 3" },
+      { id: 4, title: "Note 4", content: "Content 4" },
+      { id: 5, title: "Note 5", content: "Content 5" },
     ],
   });
 });
