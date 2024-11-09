5c41423b4427a67a1a44261339391b971f17bc0e
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-11-09 22:41:38 +0600

    feat: update api response notes

--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -11,6 +11,11 @@ notesRoutes.get("/", (c) => {
       { id: 3, title: "Note 3", content: "Content 3" },
       { id: 4, title: "Note 4", content: "Content 4" },
       { id: 5, title: "Note 5", content: "Content 5" },
+      { id: 6, title: "Note 6", content: "Content 6" },
+      { id: 7, title: "Note 7", content: "Content 7" },
+      { id: 8, title: "Note 8", content: "Content 8" },
+      { id: 9, title: "Note 9", content: "Content 9" },
+      { id: 10, title: "Note 10", content: "Content 10" },
     ],
   });
 });
